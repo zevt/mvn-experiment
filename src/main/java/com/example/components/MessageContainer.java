492a343d3d04409715adcdfeb9e7713aaeda61df
Viet Tran <vtran@Viets-MacBook-Pro.local>
2018-01-23 11:58:19 -0600

    update pom

--- src/main/java/com/example/components/MessageContainer.java
+++ src/main/java/com/example/components/MessageContainer.java
@@ -2,8 +2,6 @@ package com.example.components;
 
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 public class MessageContainer {
 
